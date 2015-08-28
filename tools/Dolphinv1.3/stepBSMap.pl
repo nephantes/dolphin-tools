a2a0f1a0e351d87d4a46b52be04321b58cac67db
Alastair Firth <alfirth@gmail.com>
2015-08-28 17:03:01 -0400

    add rmcom and retab

--- tools/Dolphinv1.3/stepBSMap.pl
+++ tools/Dolphinv1.3/stepBSMap.pl
@@ -140,81 +140,84 @@ opendir(my $dh, $inputdir) || die "can't opendir $inputdir: $!";
 my @file_list = grep { /\.fastq/ } readdir($dh);
 closedir $dh;
 if ( $spaired ) {
-  #create a hash of arrays { s1 => [s1.1.fq, s1.2.fq], s2 => [s2.1.fq],[s2.2.fq] ]
-  foreach my $file ( @file_list ) {
-    #DO NOT include .1 or .2 in the hash key
-    $file =~ m/(.*)\.([12])\.fastq/; #get the "bname" as $1 and use it as the hash key
-    #order matters, so unshift if it's .1 and push if it's .2
-    unshift @{ $files{$1} }, "$inputdir/$file" if ($2 == 1); #unshift the filename into the array $files{bname} => [filename.1.fastq ?,file2?]
-    push @{ $files{$1} }, "$inputdir/$file" if ($2 == 2); #push the filename into the array $files{bname} => [?file1,? filename.2.fastq]
-  }
+	#create a hash of arrays { s1 => [s1.1.fq, s1.2.fq], s2 => [s2.1.fq],[s2.2.fq] ]
+	foreach my $file ( @file_list ) {
+		#DO NOT include .1 or .2 in the hash key
+		$file =~ m/(.*)\.([12])\.fastq/; #get the "bname" as $1 and use it as the hash key
+		#order matters, so unshift if it's .1 and push if it's .2
+		unshift @{ $files{$1} }, "$inputdir/$file" if ($2 == 1); #unshift the filename into the array $files{bname} => [filename.1.fastq ?,file2?]
+		push @{ $files{$1} }, "$inputdir/$file" if ($2 == 2); #push the filename into the array $files{bname} => [?file1,? filename.2.fastq]
+	}
 }
 else {
-  #create a hash of filenames {s1.1 => inputdir/s1.1.fq, s1.2 => inputdir/s1.2.fq}
-  foreach my $file ( @file_list ) {
-    #DO INCLUDE .1 or .2 in the hash key, if present
-    $file =~ m/(.*)\.fastq/; #get the "bname" as $1 and use it as the hash key
-    push @{ $files{$1} }, "$inputdir/$file"; #push the filename into the array $files{bname} => [filename]
-  }
+	#create a hash of filenames {s1.1 => inputdir/s1.1.fq, s1.2 => inputdir/s1.2.fq}
+	foreach my $file ( @file_list ) {
+		#DO INCLUDE .1 or .2 in the hash key, if present
+		$file =~ m/(.*)\.fastq/; #get the "bname" as $1 and use it as the hash key
+		push @{ $files{$1} }, "$inputdir/$file"; #push the filename into the array $files{bname} => [filename]
+	}
 }
 
 ### Run the jobs ###
 foreach my $bname ( keys %files ) {
-  if ( $spaired and ( scalar @{ $files{$bname} } != 2 ) ) {
-    die "Expected paired files for $bname but found " . Dumper($files{$bname});
-  }
-  if ( ! $spaired and ( scalar @{ $files{$bname} } != 1 ) ) {
-    die "Expected single file for $bname but found " . Dumper($files{$bname});
-  }
-  do_job( $bname, @{ $files{$bname} } );
+	if ( $spaired and ( scalar @{ $files{$bname} } != 2 ) ) {
+		die "Expected paired files for $bname but found " . Dumper($files{$bname});
+	}
+	if ( ! $spaired and ( scalar @{ $files{$bname} } != 1 ) ) {
+		die "Expected single file for $bname but found " . Dumper($files{$bname});
+	}
+	do_job( $bname, @{ $files{$bname} } );
 }
 
 sub do_job {
-  my ($bname, $file1, $file2) = @_;
+	my ($bname, $file1, $file2) = @_;
 
-  #$file2 will be undef if not paired
-  die "Invalid file (must be a regular file): $file1" if ( ! -f $file1 );
-  die "Invalid file (must be a regular file): $file2" if ( $file2 and ! -f $file2 );
+	#$file2 will be undef if not paired
+	die "Invalid file (must be a regular file): $file1" if ( ! -f $file1 );
+	die "Invalid file (must be a regular file): $file2" if ( $file2 and ! -f $file2 );
 
 
 
 #construct the command
-  my $logfile = "$bname.$binname.log";
-  my $outfile = "$outdir/$bname.sorted.bam";
+	my $logfile = "$bname.$binname.log";
+	my $sortedfile = "$outdir/$bname.sorted"; #because samtools adds bam automatically
+	my $outfile = "$outdir/$bname.bam";
 	my $unsortedfile = "$outdir/$bname.unsorted";
 
 # construct the move command
-  my $mvcom = "&& mv $outfile $unsortedfile"; 
-	my $sortcom = "$args{samtools} sort $unsortedfile $outfile";
-  my $indexcom = "$args{samtools} index $outfile"; 
+	my $mvcom = "&& mv $outfile $unsortedfile";
+	my $sortcom = "&& $args{samtools} sort $unsortedfile $sortedfile";
+	my $indexcom = "&& $args{samtools} index $outfile";
+	my $rmcom = "&& rm $unsortedfile";
 
-  my $com = $args{binpath};
-  $com .= " -a $file1";
-  $com .= " -b $file2" if ( $file2 ); #only for paired end libs
-  $com .= " -o $outfile";
-  $com .= " -d $args{ref}";
+	my $com = $args{binpath};
+	$com .= " -a $file1";
+	$com .= " -b $file2" if ( $file2 ); #only for paired end libs
+	$com .= " -o $outfile";
+	$com .= " -d $args{ref}";
 	$com .= " -D $args{digestion}" if ( exists $args{digestion} );
-  $com .= " $args{params}" if ( exists $args{params} );
-  $com .= " > $logfile 2>&1";
-  $com .= " $mvcom";
-  $com .= " $sortcom";
-  $com .= " $indexcom";
+	$com .= " $args{params}" if ( exists $args{params} );
+	$com .= " > $logfile 2>&1";
+	$com .= " $mvcom";
+	$com .= " $sortcom";
+	$com .= " $indexcom";
+	$com .= " $rmcom";
 
-  print "command: $com\n" if $args{verbose};
+	print "command: $com\n" if $args{verbose};
 
 # construct the job submission command
 # jobname = servicename_bname
-  my $jobname = "$args{servicename}_$bname";
+	my $jobname = "$args{servicename}_$bname";
 
-  my $job = qq($args{jobsubmit} -n $jobname -c "$com"); #TODO $com should be single quoted?
-  print "job: $job\n" if $args{verbose};
+	my $job = qq($args{jobsubmit} -n $jobname -c "$com"); #TODO $com should be single quoted?
+	print "job: $job\n" if $args{verbose};
 
 # /share/pkg/moabs/1.3.2//bin/bsmap -a s1_r1.fq              -o wt_r1.bam   -d /share/data/umw_biocore/genome_data/mouse/mm10/mm10.fa  -R   -D C-CGG  -p 8  > moabs.bsmap.wt_r1.bam.log 2>&1
 
 # run the job
-  unless ( system ($job) == 0 ) {
-    die "Error 25: Cannot run the job $job: $!";
-  }
+	unless ( system ($job) == 0 ) {
+		die "Error 25: Cannot run the job $job: $!";
+	}
 }
 
 __END__
@@ -224,24 +227,24 @@ __END__
 
 stepBSMap.pl
 
-=head1 SYNOPSIS  
+=head1 SYNOPSIS
 
   stepBSMap.pl -binpath     bsmap binary path </path/to/bsmap>
                -digestion   restriction enzyme digestion site <C-CGG>
                -dspaired    paired end or single end <none|yes|paired>
                -jobsubmit   command to execute to submit job
-               -outdir      output directory </output/directory/> 
+               -outdir      output directory </output/directory/>
                -params      additional optional bsmap params [bsmap params]
                -previous    previous step in pipeline
                -ref         reference sequences file <fasta>
                -samtools    samtools binary location
                -servicename service name to use in job name
                -verbose     print extra debugging output [boolean]
-  
+
   stepBSMap.pl -help
-  
+
   stepBSMap.pl -version
-  
+
   For help, run this script with -help option.
 
 
@@ -267,7 +270,7 @@ command to execute to submit job
 
 =head2 -outdir
 
-output directory </output/directory/> 
+output directory </output/directory/>
 
 =head2 -params
 
@@ -304,23 +307,23 @@ stepBSMap.pl TODO
 BSMAP arguments included for reference.
 
   -a  <str>   query file, FASTA/FASTQ/BAM format.  The input format will be auto-detected. (required)
-  -b  <str>   query file b for pair end data, FASTA/FASTQ/BAM format.  The input format will be auto-detected. 
+  -b  <str>   query file b for pair end data, FASTA/FASTQ/BAM format.  The input format will be auto-detected.
               if the input format is in BAM format, it should be the same as the file specified by "-a" option.
-              BSMAP will read the two sets of reads w.r.t to the 0x40/0x80 flag in the input BAM file. 
+              BSMAP will read the two sets of reads w.r.t to the 0x40/0x80 flag in the input BAM file.
               (required for pair-end mapping)
   -d  <str>   reference sequences file, FASTA format. (required)
   -o  <str>   output alignment file, if filename has .sam suffix, the output
               will be in SAM format, if the filename has .bam suffix, the output file
-              be in sorted BAM file, and a filename.bai index file will be generated, 
+              be in sorted BAM file, and a filename.bai index file will be generated,
               for other filename suffix the output is in BSP format. (required)
-  -2  <str>   output alignment file for unpaired reads in pair end mapping, only used for BSP format output. 
-              If the output format is specified in BAM/SAM format, this option will be ignored, all alignments will be 
-              writen to one BAM/SAM output file specified by the "-o" option. 
+  -2  <str>   output alignment file for unpaired reads in pair end mapping, only used for BSP format output.
+              If the output format is specified in BAM/SAM format, this option will be ignored, all alignments will be
+              writen to one BAM/SAM output file specified by the "-o" option.
               (required for pair-end mapping with BSP format output)
   -s  <int>   seed size, default=16, min=8, max=16. (WGBS mode)
               For RRBS mode, seed length is fixed to 12 and this command line option is neglected.
               longer seed size is faster, ~1.5 times faster with each additional nt
-  -v  <int>   max number of mismatches allowed on a read, default=2, max=15, 
+  -v  <int>   max number of mismatches allowed on a read, default=2, max=15,
               usually this number should be around 10% of the read length.
   -w  <int>   max number of equal best hits to count, smaller will be faster, default=MAXHITS in makefile
   -q  <int>   quality threshold in trimming 3'end of reads, 0-40, default=0. (no trim)
@@ -333,31 +336,31 @@ BSMAP arguments included for reference.
   -L  <int>   mapping the first N nucleotide of the read, default: 0 (map the whole read).
   -I  <int>   index interval (1~16), meaning the reference genome will be indexed every Nbp, default=4. (WGBS mode)
               For RRBS mode, index_interval is fixed to 1bp and this command line option is neglected.
-              larger index interval uses memory, and slightly reduces mapping sensitivity. (~0.5% difference) 
+              larger index interval uses memory, and slightly reduces mapping sensitivity. (~0.5% difference)
               for human genome, -I 16 uses ~5GB, compared with ~9GB at the default -I 4.
   -A  <str>   set the adapter sequence(s) and trim from 3'end of reads, default=none, requires at least 4nt matched, no mismatch allowed.
-              Multiple -A options could be specified to set more than one adapter sequences, i.e. in pair-end sequencing case. 
+              Multiple -A options could be specified to set more than one adapter sequences, i.e. in pair-end sequencing case.
               default: none (no adapter trimming)
   -R          include the reference sequences as the XR:Z:<string> field in SAM output. default=do not include.
   -B  <int>   start from the Nth read or read pair, default: 1.
   -E  <int>   end at the Nth read or read pair, default: 4,294,967,295.
-              Using -B and -E options user can specify part of the input file to be mapped, so that the input file 
-              could be divided into several parts and mapped parallely over distributed system, without creating temporary files. 
-  -D  <str>   set restriction enzyme digestion site and activate reduced representation bisulfite mapping mode (RRBS mode), 
-              i.e. reads must be mapped to digestion sites, the digestion site must be palindromic, digestion position is marked by '-', 
+              Using -B and -E options user can specify part of the input file to be mapped, so that the input file
+              could be divided into several parts and mapped parallely over distributed system, without creating temporary files.
+  -D  <str>   set restriction enzyme digestion site and activate reduced representation bisulfite mapping mode (RRBS mode),
+              i.e. reads must be mapped to digestion sites, the digestion site must be palindromic, digestion position is marked by '-',
               for example: '-D C-CGG' for MspI digestion.
               default: none, meaning whole genome shot gun mapping (WGBS mode).
   -S  <int>   seed for random number generation in selecting multiple hits.  default: 0 (seed set from system clock).
-              other seed values generate pseudo random number based on read index number, so that mapping results are reproducible. 
+              other seed values generate pseudo random number based on read index number, so that mapping results are reproducible.
   -n  [0,1]   set mapping strand information:
               -n 0: only map to 2 forward strands, i.e. BSW(++) and BSC(-+)    (i.e. the "Lister protocol")
-              for PE sequencing, map read#1 to ++ and -+, read#2 to +- and --. 
+              for PE sequencing, map read#1 to ++ and -+, read#2 to +- and --.
               -n 1: map SE or PE reads to all 4 strands, i.e. ++, +-, -+, --    (i.e. the "Cokus protocol")
               default: -n 0. Most bisulfite sequencing data is generated only from forward strands.
-  -M  <str>   set the alignment information for the additional nucleotide transition. <str> is in the form of two different nucleotides, 
+  -M  <str>   set the alignment information for the additional nucleotide transition. <str> is in the form of two different nucleotides,
               the first one in the reads could be mapped to the second one in the reference sequences.
               default: -M TC, corresponds to C=>U(T) transition in bisulfite conversion.
-              example: -M GA could be used to detect to A=>I(G) transition in RNA editing. 
+              example: -M GA could be used to detect to A=>I(G) transition in RNA editing.
   -h          help
 
 
