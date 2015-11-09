24eaeb07fefb8df3d1e02bca54253e6e44678ce4
Alper Kucukural <alper.kucukural@umassmed.edu>
2015-11-09 11:20:20 -0500

    pred_job_submit_params

--- tests/pipeline/run.bash
+++ tests/pipeline/run.bash
@@ -1,2 +1,2 @@
-#python ../src/runWorkflow.py -d biocoreweb -i @INPUT=/etc -w $1 -u kucukura -o TEST2
-python ../../src/runWorkflow.py -f Docker -d biocore -w $1 -u kucukura -o /export/TEST2
+python ../../src/runWorkflow.py -f Biocore-dev -d biocore -i @INPUT=/etc -w $1 -u kucukura -o TEST2
+#python ../../src/runWorkflow.py -f Docker -d biocore -w $1 -u kucukura -o /export/TEST2
